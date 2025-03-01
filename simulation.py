54b946e691a9e6439ffcec5566a6d7e44b911c6f
earthlertkit <earthlertkit@gmail.com>
2025-03-01 17:36:48 -0500

    Minor changes

--- simulation.py
+++ simulation.py
@@ -1,20 +1,21 @@
 from quadrotor_env import QuadrotorEnv
 import numpy as np
+import mujoco
+import mujoco.viewer
+import time
 
 # Create the environment
 env = QuadrotorEnv(render_mode="human")  # Enable live rendering
-
+    
 # Reset environment
 obs, _ = env.reset()
+dt = env.model.opt.timestep
 
 # Define PID gains for position and orientation
-pid_gains = np.array([0.1, 0., 0.1, 0.1, 0., 0.1, 0.1, 0., 0.1, 0.01, 0.01]) 
+gains = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]) 
 
-# Simulate for 200 steps
-for _ in range(200):
-    obs, reward, done, _, _ = env.step(pid_gains)  # Apply PID gains as action
-    
-    if done:  # Stop if simulation ends
-        break
-
-env.close()
+# Simulate while GUI is open
+while True:
+    obs, reward, done, _, _ = env.step(gains)  # Apply PID gains as action
+    env.render()
+    time.sleep(dt)
